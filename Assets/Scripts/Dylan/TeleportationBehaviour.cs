2b7335062a252f4a7eaa0b367bf52906e47c76c6
DylanGuidry95 <guidry.dylan.95@gmail.com>
2019-04-04 11:23:21 -0500

    Basic Teleportation

---
+++ Assets/Scripts/Dylan/TeleportationBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class TeleportationBehaviour : MonoBehaviour 
+{
+	private LineRenderer TeleportationLine;
+	
+	public float MaxTeleportDistance;	
+
+	GameObject TeleportDestination;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		TeleportationLine = GetComponent<LineRenderer>();
+		MaxTeleportDistance = MaxTeleportDistance < 0 ? 1 : MaxTeleportDistance;
+	}
+
+	private void Update() 
+	{
+		TeleportationLine.SetPosition(0, transform.position);
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, transform.forward, out hit, MaxTeleportDistance))
+		{
+			if(hit.transform.GetComponent<HTC.UnityPlugin.Vive.Teleportable>())
+			{
+				if(TeleportDestination == null)
+				{
+					TeleportDestination = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+					Destroy(TeleportDestination.GetComponent<Collider>());
+				}
+				TeleportDestination.transform.position = hit.point;
+				TeleportationLine.SetPosition(1, hit.point);
+			}
+		}
+		else if(TeleportDestination != null)
+		{
+				Destroy(TeleportDestination);
+				TeleportationLine.SetPosition(1, transform.position);
+		}
+	}
+
+	public void Teleport(GameObject teleportObject)
+	{
+		if(TeleportDestination != null)
+			teleportObject.transform.position = TeleportDestination.transform.position;
+	}
+}
