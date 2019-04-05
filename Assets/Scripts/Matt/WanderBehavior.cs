4b847f213037ae8e2fcd4eb3c0fe870e110a42ac
matthewguillory65 <matthewguillory65@gmail.com>
2019-04-05 14:36:20 -0500

    NPC work
    NPC gets to destination, then turns to next destination

--- Assets/Scripts/Matt/WanderBehavior.cs
+++ Assets/Scripts/Matt/WanderBehavior.cs
@@ -28,47 +28,56 @@ namespace Assets.Scripts.Matt
         // Update is called once per frame
         void Update()
         {
-            if(nextWP == currentWP)
+            if (nextWP == currentWP)
             {
                 nextWP = Random.Range(0, WP.Count);
-                
             }
-            //npc.transform.rotation = Quaternion.RotateTowards(WP[currentWP].transform.rotation, WP[nextWP].transform.rotation, 360);
+
             if (PH == true)
             {
                 MoveToNewPosition();
             }
-            if(!agent.isStopped)
+            
+            if (npc.transform.position == WP[currentWP].transform.position)
             {
-                var targetPosition = WP[nextWP].transform.position;
-                var targetPoint = WP[nextWP].transform.position;
-                var _direction = (targetPoint - targetPosition).normalized;
-                var _lookRotation = Quaternion.LookRotation(_direction);
-
-                npc.transform.rotation = Quaternion.RotateTowards(WP[currentWP].transform.rotation, WP[nextWP].transform.rotation, 360);
+                RotateToNewPosition();
+                if(npc.transform.position.x < 1 || npc.transform.position.x > -1)
+                {
+                    RotateToNewPosition();
+                }
             }
         }
 
         public void MoveToNewPosition()
         {
-            
+
             if (nextWP != currentWP && WP.Count != 0)
             {
-                Vector3 targetDir = WP[nextWP].transform.position - npc.transform.position;
                 print("Hit new WP");
                 PH = false;
                 agent.SetDestination(WP[nextWP].transform.position);
-                float step = 2.0f * Time.deltaTime;
-                newDir = Vector3.RotateTowards(npc.transform.position, WP[nextWP].transform.position, step, 0.0f);
                 StartCoroutine(Seconds());
                 currentWP = nextWP;
             }
+        }
+
+        public void RotateToNewPosition()
+        {
+            Vector3 relP = WP[nextWP].transform.position - npc.transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relP, Vector3.up);
+            npc.transform.rotation = rotation;
+            StartCoroutine(Rotate());
         }
 
         IEnumerator Seconds()
         {
             yield return new WaitForSecondsRealtime(6);
             PH = true;
+        }
+
+        IEnumerator Rotate()
+        {
+            yield return new WaitForSecondsRealtime(4);
         }
     }
 }
