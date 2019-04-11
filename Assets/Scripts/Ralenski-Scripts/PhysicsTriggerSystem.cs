f7657d5d0c90dab2c8747551d480d0ab7d3bd83e
Ralenski Doucet <Ralenskid@gmail.com>
2019-04-11 16:24:27 -0500

    before matthew merge anh

--- Assets/Scripts/Ralenski-Scripts/PhysicsTriggerSystem.cs
+++ Assets/Scripts/Ralenski-Scripts/PhysicsTriggerSystem.cs
@@ -1,112 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Assets.Scripts.Brett;
 using UnityEngine;
-using Valve.VR;
 
 namespace Ralenski
 {
+    [CreateAssetMenu]
     public class PhysicsTriggerSystem : MonoBehaviour
     {
         public GameEvent TriggerEnter;
-
         public GameEvent TriggerExit;
-
         public GameEvent TriggerStay;
-
         void OnTriggerEnter(Collider other)
         {//the other collider's attached rigidbodies game object's name.
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
+            if (other.gameObject.name=="Player")
             {
                 TriggerEnter.Raise();
-            }
-
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
-            {
-                TriggerEnter.Raise();
-            }
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
-            {
-                TriggerEnter.Raise();
+                Debug.Log("Trigger Enter Raised");
             }
         }
 
         void OnTriggerExit(Collider other)
         {
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
+            if (other.gameObject.name == "Player")
             {
                 TriggerExit.Raise();
-            }
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
-            {
-                TriggerExit.Raise();
-            }
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
-            {
-                TriggerExit.Raise();
+                Debug.Log("Trigger Exit Raised");
             }
         }
 
         void OnTriggerStay(Collider other)
         {
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
+            if (other.gameObject.name == "Player")
             {
                 TriggerStay.Raise();
+                Debug.Log("Trigger Stay Raised");
             }
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
-            {
-                TriggerStay.Raise();
-            }
-            if (other.GetComponent<Rigidbody>().detectCollisions == true)
-            {
-                TriggerStay.Raise();
-            }
-        }
-        public struct GuardTriggerSystem
-        {
-            private PhysicsTriggerSystem PTS;
-            private GameEventListener GuardGEL;
-            public List<GameEventListener> EventList;
+         }
 
-            public void EventsToListenFor(GameEventListener listener)
-            {
-                listener.Event.Subscribe(GuardGEL);
-                EventList[1].OnEventRaised();
-            }
-        }
-        public struct PlayerTriggerSystem
-        {
-            private PhysicsTriggerSystem PTS;
-            private GameEventListener PlayerGEL;
-            public List<GameEventListener> EventList;
 
-            public void EventsToListenFor(GameEventListener listener)
-            {
-                listener.Event.Subscribe(PlayerGEL);
-                EventList[1].OnEventRaised();
-            }
-
-        }
-
-        public struct InteractableTriggerSystem
-        {
-            private PhysicsTriggerSystem PTS;
-            private LerpBehaviour LERPY;
-            private GameEventListener InterGEL;
-            public List<GameEventListener> EventList;
-
-            public void EventsToListenFor(GameEventListener listener)
-            {
-                listener.Event.Subscribe(InterGEL);
-                EventList[1].OnEventRaised();
-            }
-        }
         // Use this for initialization
         void Start()
         {
 
         }
-
         // Update is called once per frame
         void Update()
         {
---
+++ Assets/Scripts/Ralenski-Scripts/PhysicsTriggerSystemListener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Brett;
+using UnityEngine;
+
+public class PhysicsTriggerSystemListener : MonoBehaviour
+{
+    public GameEventListener PhysicsListener;
+
+    public List<GameEventListener> Listeners;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
