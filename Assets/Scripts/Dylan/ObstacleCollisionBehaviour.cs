c3bd91b74197b153298faaed465d776127f21d8e
DylanGuidry95 <guidry.dylan.95@gmail.com>
2019-04-29 12:03:51 -0500

    New Collisions

---
+++ Assets/Scripts/Dylan/ObstacleCollisionBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Dylan
+{
+	public class ObstacleCollisionBehaviour : MonoBehaviour 
+	{
+		public float bounceBack;
+
+		public Assets.Scripts.Brett.GameEvent OnObstacleCollisionEnter;
+		public Assets.Scripts.Brett.GameEvent OnObstacleCollisionExit;
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if(other.GetComponent<Camera>() != null)
+				OnObstacleCollisionEnter.Raise();
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			if(other.GetComponent<Camera>() != null)
+				OnObstacleCollisionExit.Raise();
+		}
+	}
+}
