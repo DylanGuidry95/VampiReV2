f0fb5212536b51f3fc0f37b818dd0be64e869693
bstelly <bstelly1999@gmail.com>
2019-05-02 12:14:57 -0500

    Scene transitions

---
+++ Assets/Scripts/Brett/EndSceneBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Brett;
+using UnityEngine;
+
+public class EndSceneBehaviour : MonoBehaviour
+{
+
+    public GameEvent OnFadeIn;
+    public GameEvent OnFadeOut;
+    public GameEvent OnIntroStart;
+
+    public float LogoTimer;
+
+    private float timer = 3.0f;
+    private bool isRaised = false;
+
+    public GameObject PlayerPrefab;
+
+    void Start ()
+    {
+	    OnFadeIn.Raise();
+        Instantiate(PlayerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+	
+	void Update ()
+    {
+        LogoTimer -= Time.deltaTime;
+        if (LogoTimer < 0 && !isRaised)
+        {
+            isRaised = true;
+            OnFadeOut.Raise();            
+        }
+
+        if (isRaised)
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                OnIntroStart.Raise();
+            }
+        }
+	}
+}
--- Assets/Scripts/State/GameEndState.cs
+++ Assets/Scripts/State/GameEndState.cs
@@ -14,10 +14,7 @@ namespace Assets.Scripts.Brett
 		{
 		}
 
-        private float timer = 3.0f;
-        private bool isRaised = false;
-
-        public override void Update(Context c, ConditionScriptable conditionScriptable)
+		public override void Update(Context c, ConditionScriptable conditionScriptable)
 		{
 			for (int i = 0; i < conditionScriptable.conditions.Count; i++)
 
@@ -30,14 +27,7 @@ namespace Assets.Scripts.Brett
 
 				if(conditionScriptable.conditions[i].name == "OnIntroStart" && conditionScriptable.conditions[i].isRaised)
 				{
-                    timer -= Time.deltaTime;
-
-                    if (isRaised == false)
-                    {
-                        Resources.Load<GameEvent>("Game Events/OnFadeOut").Raise();
-                    }
-                    isRaised = true;
-                    c.ChangeState(new IntroStartState());
+					c.ChangeState(new IntroStartState());
 					conditionScriptable.Toggle("OnIntroStart");
 
 
