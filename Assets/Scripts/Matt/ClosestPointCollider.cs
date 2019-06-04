86375463178a9061c45ab1a48a49efaecc2c94ad
matthewguillory65 <matthewguillory65@gmail.com>
2019-06-04 08:58:27 -0500

    Guard_AI
    Fixing the Guard's "sight"; The ability for the player to hind begind an object

---
+++ Assets/Scripts/Matt/ClosestPointCollider.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Matt
+{
+    public class ClosestPointCollider : MonoBehaviour
+    {
+        public static Vector3 closestpoint;
+        GameObject closestgameobj;
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+
+        }
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.gameObject.CompareTag("HideAble"))
+            {
+                closestgameobj = coll.gameObject;
+                Debug.Log("Something hit");
+                closestpoint = closestgameobj.transform.position;
+            }
+        }
+
+        public bool WhosCloser(GameObject player)
+        {
+            bool IsItTho = false;
+            var closercollider = Vector3.Distance(closestpoint, transform.position);
+            var closerplayer = Vector3.Distance(player.transform.position, transform.position);
+            if (closerplayer < closercollider)
+            {
+                Debug.Log("Is working");
+                IsItTho = true;
+            }
+            else
+            {
+                Debug.Log("No working");
+                IsItTho = false;
+            }
+
+            return IsItTho;
+        }
+    }
+}
+
+
