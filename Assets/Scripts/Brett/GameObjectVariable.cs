66b461d0b631e8169fc5c67000914b52a4510275
Donald <donaldg@aie.edu.au>
2019-07-02 13:01:50 -0500

    remove old assets

--- Assets/Scripts/Brett/GameObjectVariable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Variables/GameObjectVariable")]
-public class GameObjectVariable : ScriptableObject
-{
-    public GameObject Value { get; set; }
-}
